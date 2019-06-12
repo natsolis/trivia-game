7114b137084c58eb811a1900b27718c433e332cd
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-11 19:06:32 -0600

    first commit

---
+++ assests/javascript/app.js
@@ -0,0 +1,21 @@
+// Psuedocoding Trivia Game
+// Step 1: 
+//  Players will hit a start button which will automatically start the game.
+// The timer for the game will start ticking down (90 seconds). That's how much time they have to answer the questions
+
+
+
+// Step 2: 
+// Write down all the questions the player will have to answer. Players can only chose one of the choices. 
+
+
+// Step 3: 
+// Once the timer stops if the player answered all the questions correctly, get All done 
+// at the end and how many they got right. 
+
+// Step 4: 
+// If timer stops and player failed to answer all wrong, the All done will show up at the end
+// but it will show how many incorrects he or she got. 
+
+// Step 5: 
+// If player wasn't able to answer all the way through will show how many are unanswered. 
