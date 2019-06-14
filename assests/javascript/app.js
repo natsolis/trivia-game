aff136f2689375013f3e59263039bc762c4b345f
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-13 23:07:34 -0600

    fifth commit

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -20,7 +20,10 @@ var question = document.getElementById("question");
 
 var counter = document.getElementById("counter");
 
+
 var timeGauge = document.getElementById("timeGuage");
+
+var score = document.getElementById("score");
 
 var choiceA = document.getElementById("A")
 var choiceB = document.getElementById("B")
@@ -37,7 +40,7 @@ var questions = [
         choiceB: "Spain",
         choiceC: "Argentina",
         choiceD: "Uruguay",
-        answer: "C"
+        correct: "C"
 
     },
     {
@@ -46,7 +49,7 @@ var questions = [
         choiceB: "Russia",
         choiceC: "Croatia", 
         choiceD: "Argentina",
-        answer: "A"
+        correct: "A"
     },
     {
         question: "Where are the 2019 Fifa Women's World Cup being held?",
@@ -54,7 +57,7 @@ var questions = [
         choiceB: "South Korea",
         choiceC: "France",
         choiceD: "The United States",
-        answer: "C"
+        correct: "C"
     },
     {
         question: "Fifa Women's World Cup, What player has scored the most goals in a World Cup?",
@@ -62,7 +65,7 @@ var questions = [
         choiceB: "Alex Morgan",
         choiceC: "Christiane Roziera", 
         choiceD: "Beyonce",
-        answer: "B"
+        correct: "B"
     },
     {
         question: "What country has won the most Fifa World Cups?", 
@@ -70,18 +73,27 @@ var questions = [
         choiceB: "Germany",
         choiceC: "Uruguay", 
         choiceD: "Brazil",
-        answer: "D"
+        correct: "D",
 
     }
 
 ]
-// var score = 0;
-var index = questions.length - 1;
-var questionIndex = 0;
-renderQuestion()
+
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
+var count =    0;
+var questionTime = 10;
+var gaugeWidth = 150;
+var gaugeUnit = gaugeWidth/questionTime;
+var timer;
+var score = 0;
+// renderQuestion()
+
+start.addEventListener("click", startQuiz);
 
 function renderQuestion(){
-    var q = questions[score];
+    var q = questions[runningQuestion];
+    question.innerHTML = "<p>"+ q.question +"</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
@@ -89,6 +101,36 @@ function renderQuestion(){
 } 
 
 
+function startQuiz() {
+start.style.display = "none";
+renderQuestion();
+quiz.style.display= "block";
+counter();
+timer = setInterval(counter, 1000);
+
+
+clearInterval();
+}
+
+
+function counter(){
+    if (count <= questionTime){
+    counter.innerHTML = count;
+    timeGauge.style.width = count * gaugeUnit + "px" ;
+    count++;
+    }else{
+        count = 0;
+        answerIsWrong();
+        if(questionIndex < index){
+         questionIndex++;
+         question();
+        } else { clearInterval(timer)
+            score();
+        }
+
+
+    }
+}
 // for (var i = 0; i < questions.length; i++) {
 //     var response = window.prompt(questions[i].prompt);
 //     if (response == questions[i].answer) {
@@ -104,55 +146,33 @@ function renderQuestion(){
 // Step 3: 
 // Once the timer stops if the player answered all the questions correctly, get All done 
 // at the end and how many they got right. 
-var questionTime = 10;
-var gaugeWidth = 150;
-var count =    0;
-var guageProgressUnit = gaugeWidth/questionTime;
-
-function counter(){
-    if (count <= questionTime){
-    counter.innerHTML = count;
-    timeGauge.style.width = guageProgressUnit * count + "px";
-    count++;
-    }else{
-        count = 0;
-        answerIsWrong();
-        if(questionIndex < index){
-         questionIndex++;
-         question();
-        } else { clearInterval(timer)
-            score();
-        }
-
-
-    }
-}
-var timer =
-setInterval(counter, 1000);
-
-
-clearInterval();
 
 
 function checkAnswer(answer){
-    if(questions[questionIndex].correct == answer){ 
+    if(answer == questions[runningQuestion].correct){ 
         score++;
-        answerIsCorreect();
+         answerIsCorreect();
     } else{
-        answerIsWrong();
+        // answerIsWrong();
     } 
-    if(questionIndex < index){
+    if(runningQuestion < index){
         count = 0; 
-        questionIndex++;
+        runningQuestion++;
         question();
     } else{
         clearInterval(timer);
         score();
     }
 }
+function answerIsCorreect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "0f0";
+}
 // Step 4: 
 // If timer stops and player failed to answer all wrong, the All done will show up at the end
 // but it will show how many incorrects he or she got. 
-
+function score(){
+    score.Container.style.display = "block";
+    var scorePercent = Math.round(100 * score / question.length);
+}
 // Step 5: 
 // If player wasn't able to answer all the way through will show how many are unanswered. 
